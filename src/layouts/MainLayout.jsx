1f8be970bcce219e02a81cb6eec1aaa87083679d
yaxyobekb <pubgn9642@gmail.com>
2024-10-05 10:12:11 +0500

    add site nav links

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 
 // Router
-import { Outlet } from "react-router-dom";
-
-// Components
-import Header from "../components/Header";
+import { Link, Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* header */}
-      <Header />
+      <header className="pt-5">
+        <div className="flex items-center justify-center gap-5 container">
+          <Link to="/">Add</Link>
+          <Link to="/delete">Delete</Link>
+        </div>
+      </header>
 
       {/* main */}
-      <main className="">
+      <main>
         <Outlet />
       </main>
     </div>
